31b290af447114b4ca6e01778be68acdbc19ae6f
aplo <alban.ploquin@cgi.com>
2025-02-21 17:54:38 +0100

    fix(local_user): #MEN-506: add constant for datatable

---
+++ classes/utils/datatablescolumns.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace local_user;
+
+class datatablescolumns
+{
+    public const local_user_colums = [
+        0 => "u.lastname",
+        1 => "u.firstname",
+        2 => "u.email",
+        3 => "mainentity",
+        4 => "u.lastaccess",
+        'default' => "u.email"
+    ];
+}
