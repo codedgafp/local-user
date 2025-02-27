774d4ce1b469d3c9a3c03f7a1782a55255349529
Abdelilah EL OUARDI <abdelilah.elouardi@cgi.com>
2025-02-27 11:05:37 +0100

    fix(space-users): #MEN-506 order by entity idnumber

--- classes/utils/datatablescolumns.php
+++ classes/utils/datatablescolumns.php
@@ -8,7 +8,7 @@ class datatablescolumns
         0 => "u.lastname",
         1 => "u.firstname",
         2 => "u.email",
-        3 => "idnumber",
+        3 => "e.idnumber",
         4 => "u.lastaccess",
         'default' => "u.email"
     ];
